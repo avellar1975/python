17fa503c7867105d380df401b7020a75b28bbfee
Evandro <evandro.avellar@gmail.com>
2018-12-15 18:30:22 -0200

    Versão com while

--- adivinhacao.py
+++ adivinhacao.py
@@ -3,16 +3,27 @@ print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
 numero_secreto = 42
+tentativas = 3
+rodada =1
 
-chute_str = input("Digite o seu número: ")
+while(rodada <= tentativas):
+    print("Tentativa {} de {}"py.format(rodada,tentativas))
+    chute_str = input("Digite o seu número: ")
+    print("Você digitou: ", chute_str)
+    chute = int(chute_str)
 
-print("Você digitou: ", chute_str)
+    acertou = numero_secreto == chute
+    maior   = chute > numero_secreto
 
-chute = int(chute_str)
+    if (acertou):
+        print("Você acertou!")
+        break
+    else:
+        if(maior):
+            print("O seu chute foi maior que o número secreto!")
+        else:
+            print("O seu chute foi menor que o número secreto!")    
 
-if (numero_secreto == chute):
-    print("Você acertou!")
-else:
-    print("Você errou!")
-    
+    rodada = rodada + 1
+        
 print("Fim do jogo")
