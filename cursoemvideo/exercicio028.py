bf53ad4f6052994a96a9cbe8462da8422977b211
Evandro <evandro.avellar@gmail.com>
2019-01-08 20:14:31 -0200

    Inclusão de sleep()
    Import time
    time.sleep(3)

--- cursoemvideo/exercicio028.py
+++ cursoemvideo/exercicio028.py
@@ -1,6 +1,23 @@
 # Desafio 028
 
 import random
+import time
+
+n = random.random()
+secreto = int(n * 100 % 6)
+
+x = int(input('Digite um número inteiro de 0 a 5: '))
+
+print('Processando ...')
+time.sleep(3)
+
+if(x == secreto):
+    print('Você venceu!!!')
+else:
+    print('O número secreto era {}. Você perdeu!!!'.format(secreto))
+# Desafio 028
+
+import random
 
 n = random.random()
 secreto = int(n * 100 % 6)
