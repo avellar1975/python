5e087658f7051eca55e2881c722e4b663072fc41
Evandro <evandro.avellar@gmail.com>
2019-04-15 18:31:01 -0300

    Update rename.py

--- rename.py
+++ rename.py
@@ -7,6 +7,7 @@
 # Adiciona dosiero_ nos nomes dos arquivos
 # o parametro filename[:-4] retira os quatro útimos caracteres
 # o parametro filename[5:] retira os 5 primeiros caracteres
+# Importante que o arquivo rename.py esteja dentro do diretório
 
 # Importando biblioteca os
 import os
@@ -15,7 +16,7 @@ for filename in os.listdir("."):
     if filename.startswith("span_"):
         os.rename(filename, filename[5:])
 
-for nome in os.listdir("."):
-        if nome[-2:] != 'py':
-            new_filename = 'dosiero_' + nome
-            os.rename(nome,new_filename)
+for nomo in os.listdir("."):
+        if nomo[-2:] != 'py':
+            nova_nomo = 'dosiero_' + nomo
+            os.rename(nomo,nova_nomo)
